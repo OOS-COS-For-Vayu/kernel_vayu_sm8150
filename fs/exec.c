73f57ec877fade9f69fca56ef2dfb99c01549eb6
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-03-24 10:39:17 +0800

    fs: kernelsu: scope-minimized manual hooks
    This refactors original KSU hooks to replace deep kernel function hooks with targeted hooks.
    This backports KernelSU pr#1657 and having pr#2084 elements (32-bit sucompat).
    It reduces the scope of kernel function interception and still maintains full fucntionality.
    
    more info: https://github.com/backslashxx/KernelSU/issues/5

--- fs/exec.c
+++ fs/exec.c
@@ -1898,6 +1898,13 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1920,6 +1927,12 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	else
+		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1963,6 +1976,10 @@ static int compat_do_execveat(int fd, struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+#ifdef CONFIG_KSU
+	if (!ksu_execveat_hook)
+		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL); /* 32-bit su */
+#endif
 	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 #endif
