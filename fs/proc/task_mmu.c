63f40fe2654c37e8c7964bb8eac2effcfef515cd
momenabdulrazek <momen.abdelrazek33@gmail.com>
2025-06-21 19:36:13 +0300

    Revert "susfs4ksu: fixup"
    This reverts commit dc266660a39405ce1942315f969b0ae5f024b68f.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -20,9 +20,6 @@
 #include <linux/uaccess.h>
 #include <linux/mm_inline.h>
 #include <linux/ctype.h>
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-#include <linux/susfs_def.h>
-#endif
 
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 #include <linux/susfs_def.h>
