23a29f5bb011c82358b6306bdb6f33cba88b262e
momenabdulrazek <momen.abdelrazek33@gmail.com>
2025-07-11 16:54:19 +0300

    Add non declared "susfs_is_current_non_root_user_app_proc & susfs_set_current_non_root_user_app_proc"

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -53,6 +53,8 @@
 #define INODE_STATE_SUS_KSTAT BIT(26)
 #define INODE_STATE_OPEN_REDIRECT BIT(27)
 
+#define TIF_NON_ROOT_USER_APP_PROC 33 // thread_info->flags is unsigned long :D
+
 #define TASK_STRUCT_NON_ROOT_USER_APP_PROC BIT(24)
 
 #define AS_FLAGS_ANDROID_DATA_ROOT_DIR 28
@@ -71,5 +73,15 @@
 #define DATA_ADB_NO_AUTO_ADD_SUS_BIND_MOUNT "/data/adb/susfs_no_auto_add_sus_bind_mount"
 #define DATA_ADB_NO_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT "/data/adb/susfs_no_auto_add_sus_ksu_default_mount"
 #define DATA_ADB_NO_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT "/data/adb/susfs_no_auto_add_try_umount_for_bind_mount"
+
+
+static inline bool susfs_is_current_non_root_user_app_proc(void) {
+	return test_ti_thread_flag(&current->thread_info, TIF_NON_ROOT_USER_APP_PROC);
+}
+
+static inline void susfs_set_current_non_root_user_app_proc(void) {
+	set_ti_thread_flag(&current->thread_info, TIF_NON_ROOT_USER_APP_PROC);
+}
+
 
 #endif // #ifndef KSU_SUSFS_DEF_H
