44ed2100d2030d0c0300be995659520342ce6afa
simonpunk <simonpunk2016@gmail.com>
2026-09-13 16:57:03 +0800

    fs: susfs: Refactor spoofing logic of SUS_MOUNT, SUS_KSTAT and OPEN_REDIRECT
    /* Objective */
    - This commit contains two parts:
    1. SUS_MOUNT no longer relies on kernel_umount. we no longer need to add sus mounts to kernel_umount, which means we can disable kernel_umount in KSU manager and mounts are still hidden, but it is still safe to have them umounted. Besides, we can save a lot of overheads where we have to umount those sus mounts after zygote unshares.
    
    2. Make SUS_MOUNT, SUS_KSTAT and OPEN_REDIRECT work independently without interfering with each other, while they can also work all together to achieve other types of hiding like when the mounted/redirected path needs to be visible to app process.
    
    /* kernel changes */
    [SUS_MOUNT]
    - Now SUS_MOUNT will be always effective for process that is marked with TIF_PROC_UMOUNTED.
    
    - Spoofing __lookup_mnt() for zygote spawned app process as well, so process that is marked with TIF_PROC_MOUNTED will only reach the non-sus-mounted path without having them umounted, no matter it is spwaned by zygote or zygote_next or not.
    
    - Change all "static_branch_unlikely" instances in proc_namespace.c to "static_branch_likely" since we need to have it enabled most of the time anyway.
    
    [SUS_KSTAT]
    - Now SUS_KSTAT will be always effective for process with (uid % 100000) >= 10000. Since sometimes we need the sus mounts or redirected path to stay for target app process, we can only rely on SUS_KSTAT to hide the stat for the mounted/redirected path.
    
    - Log the target_dev and spoofed_mnt_id for new entry as well.
    
    - Expand SUS_KSTAT_HLIST bucket size to 14bits, to make sure we get the fatest lookup as much as possible as we use only hash_for_each_possible() anyway, and a 128 KB memory cost is not a concern.
    
    - Place hlist_node and the lookup key "target_ino" at the top of the struct, to make sure it pulls 1 cache line into L1 cache.
    
    [OPEN_REDIRECT]
    - For added redirected paths which are also visible to (uid % 100000) >= 10000, SUS_KSTAT is required to hide from other detections since OPEN_REDIRECT itself cannot fully spoof the kstat, kstatfs, inotify fdinfo and proc fd.
    
    - Expand OPEN_REDIRECT_HLIST bucket size to 14bits.
    
    - Place hlist_node and the lookup key "target_ino" at the top of the struct.
    
    Note: this is just a semantic port as most of the functions and symbols (e.g STATX_MNT_ID, zygote_next, etc) did not exist in 4.14.
    
    co-authored-by: sidex15 <24408329+sidex15@users.noreply.github.com>

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -94,11 +94,13 @@ struct st_susfs_sus_kstat {
 };
 
 struct st_susfs_sus_kstat_hlist {
+	struct hlist_node                       node;
 	unsigned long                           target_ino;
 	unsigned long                           target_dev;
+	struct kstatfs                          spoofed_kstatfs;
+	int                                     spoofed_mnt_id;
 	bool                                    is_fuse;
 	struct st_susfs_sus_kstat               info;
-	struct hlist_node                       node;
 };
 #endif
 
@@ -151,6 +153,7 @@ struct st_susfs_open_redirect {
 };
 
 struct st_susfs_open_redirect_hlist {
+	struct hlist_node                       node;
 	unsigned long                           target_ino;
 	unsigned long                           target_dev;
 	unsigned long                           redirected_ino;
@@ -159,7 +162,6 @@ struct st_susfs_open_redirect_hlist {
 	struct kstatfs                          spoofed_kstatfs;
 	struct st_susfs_open_redirect           info;
 	bool                                    reversed_lookup_only;
-	struct hlist_node                       node;
 };
 #endif
 
@@ -228,6 +230,12 @@ void susfs_add_sus_kstat(void __user **user_info);
 void susfs_update_sus_kstat(void __user **user_info);
 void susfs_sus_ino_for_generic_fillattr(unsigned long ino, struct kstat *stat);
 void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);
+bool susfs_is_inode_sus_kstat(struct inode *inode, bool *out_is_fuse);
+void susfs_generic_fillattr_spoofer(struct inode *inode, struct kstat *stat, u32 result_mask);
+void susfs_show_map_vma_spoofer(struct inode *inode, dev_t *out_dev, unsigned long *out_ino);
+int susfs_sus_kstat_spoof_vfs_statfs(struct inode *inode, struct kstatfs *buf, bool *is_fuse);
+void susfs_sus_kstat_spoof_inotify_fdinfo(unsigned long *out_target_ino, dev_t *out_target_dev);
+void susfs_sus_kstat_spoof_proc_fd_seq_show(int *out_target_mnt_id, unsigned long *out_target_ino, dev_t target_dev);
 #endif
 /* try_umount */
 #ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
